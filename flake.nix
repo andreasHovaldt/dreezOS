58d3d8ab30999916b6a3cc39fbfaac64efd1b382
andreasHovaldt <andreas.hovaldt@gmail.com>
2025-02-24 00:45:45 +0100

    Moved nix config to flake

---
+++ flake.nix
@@ -0,0 +1,44 @@
+# Taken from: https://www.youtube.com/watch?v=rEovNpg7J0M
+# Build system: sudo nixos-rebuild switch --flake ~/mysystem/#dreezOS
+
+{
+  description = "My basic system flake";
+
+  inputs = {
+    #nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+
+      pkgs = import nixpkgs {
+        inherit system;
+
+        config = {
+          allowUnfree = true;
+        };
+      };
+
+    in
+    {
+
+      nixosConfigurations = {
+        dreezOS = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs system; };
+
+          modules = [
+            ./nixos/configuration.nix
+          ];
+        };
+      };
+
+    };
+}
---
+++ nixos/home.nix
@@ -0,0 +1,88 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "dreezy";
+  home.homeDirectory = "/home/dreezy";
+
+  home.stateVersion = "24.11"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = [
+    pkgs.hello
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = { };
+
+  programs.git = {
+    enable = true;
+    userName = "andreasHovaldt";
+    userEmail = "andreas.hovaldt@gmail.com";
+    aliases = {
+      pu = "push";
+      co = "checkout";
+      cm = "commit";
+      sta = "status";
+    };
+    extraConfig = {
+      core.editor = "code";
+      pull.rebase = true;
+    };
+  };
+
+  programs.ghostty = {
+    enable = true;
+
+    settings = {
+      clipboard-read = "allow";
+      clipboard-write = "allow";
+      copy-on-select = true;
+      background-opacity = 0.85;
+      background-blur = 20;
+      mouse-hide-while-typing = true;
+
+      window-theme = "dark";
+      theme = "tokyonight";
+      #theme = "midnight-in-mojave";
+      #theme = "iTerm Smoooooth";
+      minimum-contrast = 2;
+      background = "#1a1b26";
+      foreground = "#c0caf5";
+      selection-background = "#33467c";
+      selection-foreground = "#c0caf5";
+      cursor-color = "#c0caf5";
+      cursor-text = "#15161e";
+      cursor-style = "bar";
+      palette = [
+        "0=#15161e"
+        "1=#f7768e"
+        "2=#9ece6a"
+        "3=#e0af68"
+        "4=#7aa2f7"
+        "5=#bb9af7"
+        "6=#7dcfff"
+        "7=#a9b1d6"
+        "8=#414868"
+        "9=#f7768e"
+        "10=#9ece6a"
+        "11=#e0af68"
+        "12=#7aa2f7"
+        "13=#bb9af7"
+        "14=#7dcfff"
+        "15=#c0caf5"
+        "16=#3ddbd9"
+      ];
+    };
+  };
+
+  home.sessionVariables = {
+    EDITOR = "nano";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
