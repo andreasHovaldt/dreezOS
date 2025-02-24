994bbd3403787a1c1d595cfed083d877f93c32ca
andreasHovaldt <andreas.hovaldt@gmail.com>
2025-02-24 16:19:43 +0100

    Added bash aliases

--- nixos/home.nix
+++ nixos/home.nix
@@ -6,17 +6,35 @@
   home.username = "dreezy";
   home.homeDirectory = "/home/dreezy";
 
-  home.stateVersion = "24.11"; # Please read the comment before changing.
-
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = [
-    pkgs.hello
+  nixpkgs.config.allowUnfree = true;
+  home.packages = with pkgs; [
+    discord
+    vlc
+    vscode
+    evince
+    spotify
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = { };
+
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      # Wireguard
+      dreez_connect = "sudo wg-quick up /root/wireguard-keys/dreez-wg0.conf";
+      dreez_disconnect = "sudo wg-quick down /root/wireguard-keys/dreez-wg0.conf";
+
+      # AAU VPN
+      aau-vpn = "sudo openconnect --protocol=anyconnect SSL-VPN1.AAU.DK";
+
+      # Git
+      gsta = "git status";
+    };
+  };
 
   programs.git = {
     enable = true;
@@ -82,6 +100,8 @@
   home.sessionVariables = {
     EDITOR = "nano";
   };
+
+  home.stateVersion = "24.11"; # Please read the comment before changing.
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
