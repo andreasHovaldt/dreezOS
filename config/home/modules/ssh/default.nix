85b9885912444eb75d41db1a487f621100e0f65c
andreasHovaldt <andreas.hovaldt@gmail.com>
2025-04-01 11:42:32 +0200

    Created a auto ssh key script

--- config/home/modules/ssh/default.nix
+++ config/home/modules/ssh/default.nix
@@ -4,6 +4,7 @@ let
   dependencies = with pkgs; [ ];
 
   sshConfig = "${config.home.homeDirectory}/.config/ssh/config";
+  sshAddKeys = "${config.home.homeDirectory}/.config/ssh/add-keys";
 in
 {
   options = {
@@ -12,9 +13,16 @@ in
 
   config = lib.mkIf cfg.enable {
 
-    home.file.".config/ssh/config" = {
-      source = ./config;
+    home.file = {
+      ".config/ssh/config" = {
+        source = ./config;
+      };
+      ".config/ssh/add-keys" = {
+        source = ./add-keys;
+        executable = true;
+      };
     };
+
 
     # https://mynixos.com/home-manager/options/programs.ssh
     programs.ssh = {
@@ -22,6 +30,22 @@ in
       addKeysToAgent = "yes"; # Adds newly used keys to the agent for future use 
       forwardAgent = true; # https://docs.github.com/en/authentication/connecting-to-github-with-ssh/using-ssh-agent-forwarding
       includes = [ sshConfig ];
+    };
+
+    systemd.user.services = {
+      ssh-auto-key = {
+        Unit = {
+          Description = "Auto add ssh keys to the agent";
+        };
+        Service = {
+          Type = "oneshot";
+          ExecStart = ''
+            /bin/sh ${sshAddKeys}
+          '';
+          StandardOutput = "journal";
+          StandardError = "journal";
+        };
+      };
     };
 
     # Install dependencies
