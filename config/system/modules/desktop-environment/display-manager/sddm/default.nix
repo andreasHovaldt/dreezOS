34b88894cd574bb3417414059adff9cd31666a86
andreasHovaldt <andreas.hovaldt@gmail.com>
2025-04-07 18:05:32 +0200

    Added basic theme to sddm

--- config/system/modules/desktop-environment/display-manager/sddm/default.nix
+++ config/system/modules/desktop-environment/display-manager/sddm/default.nix
@@ -1,11 +1,8 @@
 { pkgs, lib, config, ... }:
 let
-  dependencies = with pkgs; [
-    # sddm-sugar-dark dependencies
-    # libsForQt5.qt5.qtquickcontrols2
-    # libsForQt5.qt5.qtgraphicaleffects
-    # libsForQt5.qt5.qtsvg
-  ];
+  username = "dreezy";
+  staticWallpaper = config.home-manager.users.${username}.wpaperd.staticWallpaper;
+  globalWallpaper = "sddm/background.jpg"; # Gets written to "/etc/<globalWallpaper>"
 in
 {
   options = {
@@ -13,19 +10,40 @@ in
   };
 
   config = lib.mkIf config.sddm.enable {
+    
+    # Make background available at boot
+    environment.etc."${globalWallpaper}".source = staticWallpaper;
 
     # Enable the X11 windowing system.
     services.xserver.enable = true;
+
+
+    # Notes regarding better theme
+    # - https://github.com/Keyitdev/sddm-astronaut-theme
+    # - https://github.com/Keyitdev/sddm-astronaut-theme/blob/master/Themes/astronaut.conf
+    # - https://github.com/DaniD3v/system-dotfiles/blob/master/common%2Fsddm.nix
+    # - https://github.com/NixOS/nixpkgs/issues/343702
+    
 
     # Enable the sddm display manager.
     services.displayManager.sddm = {
       enable = true;
       wayland.enable = true;
-      settings.General.DisplayServer = "wayland";
-      #theme = "${import ./themes/sddm-sugar-dark.nix { inherit pkgs; }}";
+      package = pkgs.kdePackages.sddm; # Use qt6 version of sddm
+      enableHidpi = true;
+      theme = "catppuccin-mocha";
+      #settings.General.DisplayServer = "wayland";
     };
 
     # Install dependencies
-    environment.systemPackages = dependencies;
+    environment.systemPackages = [(
+      pkgs.catppuccin-sddm.override {
+        flavor = "mocha";
+        font  = "Noto Sans";
+        fontSize = "9";
+        background = "/etc/${globalWallpaper}"; # FIXME: Doesn't get applied?
+        loginBackground = true;
+      }
+    )];
   };
 }
