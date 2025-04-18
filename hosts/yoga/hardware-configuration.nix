982bd58da13fc01b464c2f5e9a582e50b35b38a0
andreasHovaldt <andreas.hovaldt@gmail.com>
2025-04-18 12:42:04 +0200

    Reinstalled nixos :)

--- hosts/yoga/hardware-configuration.nix
+++ hosts/yoga/hardware-configuration.nix
@@ -5,8 +5,7 @@
 
 {
   imports =
-    [
-      (modulesPath + "/installer/scan/not-detected.nix")
+    [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usb_storage" "sd_mod" ];
@@ -15,14 +14,12 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    {
-      device = "/dev/disk/by-uuid/a12672a4-c9b0-4f24-8c19-cbc6972db52b";
+    { device = "/dev/disk/by-uuid/afeeb456-7550-4d50-939f-35ec739d6467";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    {
-      device = "/dev/disk/by-uuid/ECA9-C86B";
+    { device = "/dev/disk/by-uuid/ECA9-C86B";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
